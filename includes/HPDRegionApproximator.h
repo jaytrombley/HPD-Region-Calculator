f50caa1659eb6ff90cbb9566efe5d7292138b48b
Jay <jtrom@umich.edu>
2026-09-07 22:38:00 -0400

    add HPD Region Approximation code file

---
+++ includes/HPDRegionApproximator.h
@@ -0,0 +1,14 @@
+#ifndef HPD_REGION_APPROXIMATOR
+#define HPD_REGION_APPROXIMATOR
+
+#include <assert.h>
+#include <cmath>
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+class Dirichlet3D
+{
+    
+}
