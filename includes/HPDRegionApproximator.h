b61b03d9818ef127dfb780941d454dc978a1d278
Jay <jtrom@umich.edu>
2026-09-07 23:06:05 -0400

    add protected variables to Dirichlet3D base class

--- includes/HPDRegionApproximator.h
+++ includes/HPDRegionApproximator.h
@@ -10,5 +10,24 @@
 
 class Dirichlet3D
 {
-    
-}
+    protected:
+
+        std::vector<double> v1 { 1.0, 0.0 };
+        std::vector<double> v2 { 0.5, sqrt(3)/2 };
+        std::vector<double> v3 { 0.0, 0.0 };
+
+        double mx_1 { -sqrt(3) };
+        double mx_2 { sqrt(3) };
+        double mx_3 { 0.0 };
+        double b_1 { sqrt(3) };
+        double b_2 { 0.0 };
+        double b_3 { 0.0 };
+
+    public:
+
+        Dirichlet3D() {
+
+        }
+};
+
+#endif
