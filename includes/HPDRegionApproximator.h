d8c7055343a820b73c5cfde438790ce2a5de31fb
Jay <jtrom@umich.edu>
2026-09-07 23:30:35 -0400

    add Dirichlet3D base class function to calculate covariance matrix given parameter inputs

--- includes/HPDRegionApproximator.h
+++ includes/HPDRegionApproximator.h
@@ -51,6 +51,27 @@ class Dirichlet3D
 
             return var;
         }
+
+        std::vector<std::vector<double>> calcDirichlet3dCov (double a1, double a2, double a3)
+        {
+            double a0{a1 + a2 + a3};
+            double denom{a0 * a0 * (a0 + 1)};
+            std::vector<std::vector<double>> cov(3, std::vector<double>(3));
+
+            cov[0][0] = (a1 * (a0 - a1)) / denom;
+            cov[1][1] = (a2 * (a0 - a2)) / denom;
+            cov[2][2] = (a3 * (a0 - a3)) / denom;
+
+            cov[0][1] = (-a1 * a2) / denom;
+            cov[0][2] = (-a1 * a3) / denom;
+            cov[1][2] = (-a2 * a3) / denom;
+
+            cov[1][0] = cov[0][1];
+            cov[2][0] = cov[0][2];
+            cov[2][1] = cov[1][2];
+
+            return cov;
+        }
 };
 
 #endif
